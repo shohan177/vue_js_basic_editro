5b4bdbe83bd0b7ce1875e3a3c71f948d01a4dafa
shohan177 <sarwar.shohan24@gmail.com>
2021-04-30 00:28:51 +0600

    project file load

---
+++ assets/js/custome_script.js
@@ -0,0 +1,22 @@
+
+
+let section = new Vue({
+    el:'.section',
+    data:{
+        title: 'this is title',
+        content: 'your content here',
+        btn:{
+            title: 'read more',
+            color: 'warning',
+            outline: null,
+            
+        },
+        font:{
+            size: 20,
+            mergin: 20,
+            padding: 10,
+            color: '1E1E1E'
+        }
+        
+    }
+})
